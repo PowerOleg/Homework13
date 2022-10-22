ad477c880aae9acce6843dfc9a977487c93020a8
PowerOleg <geraskin.oleg@mail.ru>
2022-10-22 13:15:27 +0500

    Longer branch

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] sales;
-    SalesManager salesManager = new SalesManager(sales = new int[]{ 100, 20000, 0, 10000});
+        long[] sales;
+    SalesManager salesManager = new SalesManager(sales = new long[]{ 100, 20000, 0, 1_000_000_000});
     System.out.println(Arrays.toString(sales));
     System.out.printf("Max value: %d\n", salesManager.max());
     }

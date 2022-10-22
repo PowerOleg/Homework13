91148e8523b687afe46bfffe5be97dd4ac946f7a
PowerOleg <geraskin.oleg@mail.ru>
2022-10-22 15:00:39 +0500

    Stat branch

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] sales;
-    SalesManager salesManager = new SalesManager(sales = new int[]{ 100, 20000, 0, 10000});
-    System.out.println(Arrays.toString(sales));
-    System.out.printf("Max value: %d\n", salesManager.max());
+    SalesManager salesManager = new SalesManager(sales =
+            new int[]{ 1_000_000_000, 100, 20000, 0 });
+        System.out.println(Arrays.toString(sales));
+        System.out.printf("Max value: %d\n", salesManager.max());
+        System.out.printf("Average value: %d\n", salesManager.averageSale());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SalesManager {
     protected int[] sales;
 
@@ -13,5 +15,37 @@ public class SalesManager {
             }
         }
         return max;
+    }
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int averageSale() {
+        int offset = 0;
+        int max = max();
+        int min = min();
+        for (int i = 0; i < sales.length; i++) {
+        if (sales[i] == min) {
+            sales[i] = 0;
+            offset++;
+            } else if (sales[i] == max) {
+            sales[i] = 0;
+            offset++;
+        } else {
+            sales[i - offset] = sales[i];
+        }
+        }
+        int[] newSales = Arrays.copyOf(sales, (sales.length - offset));
+        int average = 0;
+        for (int sale : newSales) {
+            average += sale;
+        }
+        return average / newSales.length;
     }
 }
